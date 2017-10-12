0417f2d9cf88a2573d01fd3fcdb27ce56eec1cf1
payssion <github@payssion.com>
2017-10-12 22:26:39 +0800

    add more Malaysia banks including cimb, hlb, rhb, amb, affinepg

---
+++ app/code/Payssion/Payment/Model/Paymentmethod/HLBmy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Payssion All rights reserved.
+ */
+
+namespace Payssion\Payment\Model\Paymentmethod;
+
+class HLBmy extends PaymentMethod
+{
+	protected $_code = 'payssion_payment_hlb_my';
+}
