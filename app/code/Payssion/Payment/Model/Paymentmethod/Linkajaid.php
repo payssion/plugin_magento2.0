225c40e3329e75ebe063e860cadf0645ead17686
payssion <liudong4u@gmail.com>
2023-12-13 14:42:50 +0800

    add creditcard_id, dana_id, linkaja_id, ovo_id, qris_id, shopeepay_id

---
+++ app/code/Payssion/Payment/Model/Paymentmethod/Linkajaid.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Payssion All rights reserved.
+ */
+
+namespace Payssion\Payment\Model\Paymentmethod;
+
+class Linkajaid extends PaymentMethod
+{
+	protected $_code = 'payssion_payment_linkaja_id';
+}
